d93a5fddefad323505b4e5eea5d05ed1602114b0
15wakefieldjo <62610546+15wakefieldjo@users.noreply.github.com>
2020-04-07 22:09:00 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -2,15 +2,17 @@
 
 namespace Insertion
 {
+
     class SortingList
     {
         private int[] list;
+        internal int length;
         public readonly int Length;
 
         public SortingList(int requiredLength)
         {
             Length = requiredLength;
-            list = new int[_____];
+            list = new int[10];
         }
 
         public int getListItem(int i)
@@ -33,32 +35,34 @@ namespace Insertion
 
         public void Print()
         {
-            foreach (int i in ____)
+            foreach (int i in 10) 
                 Console.Write(String.Format("{0}, ", i));
             Console.WriteLine("");
         }
 
         public void Insert(int value)
-        {            
+        {
             /* First, find the right place for the item to go in */
-            for (int i = 0; i < list.______; i++)
+            for (int i = 0; i < list.Length; i++) ;
             {
+                int i = 0;
                 if (list[i] == 0)
                 {
                     /* There is nothing in this place, so just take it */
-                    list[_] = value;
-                    break;
+                    list[1] = value;
+
                 }
                 if (value < list[i])
                 {
+
                     /* We need to shift everything along one, so start at 
-                     * the end and copy the whole list along
-                     */
+* the end and copy the whole list along
+*/
                     for (int j = list.Length - 1; j > i; j--)
                         list[j] = list[j - 1];
 
                     list[i] = value;
-                    break;
+
                 }
             }
             this.Print();
@@ -96,25 +100,28 @@ namespace Insertion
     }
     class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args) 
         {
             char response;
-            
+
             Console.WriteLine("Please let me know what kinda sort you would like (i)nsertion sort, or (b)ubble sort, or (q)uit");
-            for(; ; )
+            for (; ; )
             {
                 response = Console.ReadKey().KeyChar;
                 switch (response)
                 {
-                    case 'i': case 'I':
+                    case 'i':
+                    case 'I':
                         InsertionSort();
                         break;
-                    case 'b': case 'B':
+                    case 'b':
+                    case 'B':
                         BubbleSort();
                         break;
-                    case 'q': case 'Q':
+                    case 'q':
+                    case 'Q':
                         return;
-                        /* NOTREACHED */
+                    /* NOTREACHED */
                     default:
                         Console.WriteLine("Um, what are you playing at?");
                         break;
@@ -122,28 +129,93 @@ namespace Insertion
             }
         }
 
-        static void BubbleSort()
+        private static void InsertionSort()
         {
-            // Do the BubbleSort dance here!
+            throw new NotImplementedException();
         }
 
-        static void InsertionSort() {
-            var unsortedList = new SortingList(10);
-            var sortedList = new SortingList(unsortedList.Length);
-            
-            /* Let's fill the unsorted list with all sorts of junk */
-            unsortedList._________();
-            unsortedList.Print();
+        static void BubbleSort() { }
+    }
 
-            /* This is where we do the insertion sort-- doesn't
-             * object oriented make this look easy? */
 
-            for (int i = 0; i < unsortedList.______; i++)
+    namespace SamplePrograms
+    {
+        class Program
+        {
+
+
+            private static int[] unsortedList, sortedList;
+            private static Random random = new Random();
+
+            static void Main(string[] args)
             {
-                sortedList.Insert(unsortedList.Pop());
+                int i;
+                unsortedList = new int[10];
+                sortedList = new int[10];
+
+                for (i = 0; i < unsortedList.Length; i++)
+                    unsortedList[i] = random.Next(50);
+                Console.WriteLine("Unsorted List:\n");
+                /* You can also do Python-style for loops */
+                foreach (int num in unsortedList)
+                    Console.WriteLine(num);
+
+                bubbleSort();
+
+                Console.ReadKey();
             }
 
-            sortedList._____();
+            /* A void function is a procedure- it does not return a value */
+            private static void bubbleSort()
+            {
+                int i, j, temp;
+
+                unsortedList.CopyTo(sortedList, 0);
+
+                for (i = 0; i < sortedList.Length; i++)
+                {
+                    for (j = 0; j < sortedList.Length - 1; j++)
+                    {
+                        if (sortedList[j] > sortedList[j + 1])
+                        {
+                            temp = sortedList[j];
+                            sortedList[j] = sortedList[j + 1];
+                            sortedList[j + 1] = temp;
+
+                        }
+                        continue;
+                    }
+                }
+                Console.WriteLine("\nSorted List:\n");
+                for (i = 0; i < 10; i++)
+                {
+
+                    Console.WriteLine(sortedList[i]);
+
+                }
+            }
+
+
+
+            private static void InsertionSort()
+            {
+                var unsortedList = new SortingList(10);
+                var sortedList = new SortingList(unsortedList.Length);
+
+                /* Let's fill the unsorted list with all sorts of junk */
+                unsortedList.Randomise();
+                unsortedList.Print();
+
+                /* This is where we do the insertion sort-- doesn't
+                 * object oriented make this look easy? */
+                 
+                for (int i = 0; i < unsortedList.length; i++)
+                {
+                    sortedList.Insert(unsortedList.Pop());
+                }
+
+                sortedList.Print();
+            }
         }
     }
 }
